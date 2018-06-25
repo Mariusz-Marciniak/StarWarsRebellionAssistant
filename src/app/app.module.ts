35e8fa68496686c05a6732c631c7568798ac6eb6
mariusz <marcinka2205@o2.pl>
2018-06-25 07:22:21 +0200

    initial version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SystemsComponent } from './systems/systems.component';
+import { WizardComponent } from './wizard/wizard.component';
+import { SelectOneComponent } from './select-one/select-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SystemsComponent,
+    WizardComponent,
+    SelectOneComponent
   ],
   imports: [
     BrowserModule

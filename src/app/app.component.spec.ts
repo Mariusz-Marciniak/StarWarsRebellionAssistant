c3d9683a9d369b59dc9dec9e72a807b643639d22
mariusz <marcinka2205@o2.pl>
2019-03-30 16:49:37 +0100

    fixed tests, changed icon

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { WizardComponent } from './wizard/wizard.component';
+import { SystemsComponent } from './systems/systems.component';
+import { SelectOneComponent} from './select-one/select-one.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        WizardComponent,
+        SystemsComponent,
+        SelectOneComponent
       ],
     }).compileComponents();
   }));
@@ -13,7 +20,7 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Star Wars Rebellion boardgame assistant'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Star Wars Rebellion boardgame assistant');
